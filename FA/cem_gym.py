2191b7583c603e624c43436169c95ca0d68bd065
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-02-23 11:22:13 +0300

    refactoring

--- FA/cem_gym.py
+++ FA/cem_gym.py
@@ -25,31 +25,6 @@ def plot_unimetric(history, metric, save_dir):
                 format='png', dpi=300)
 
 
-def make_epsilon_greedy_policy(estimator, epsilon, nA):
-    """
-    Creates an epsilon-greedy policy based on a given Q-function approximator and epsilon.
-
-    Args:
-        estimator: An estimator that returns q values for a given state
-        epsilon: The probability to select a random action . float between 0 and 1.
-        nA: Number of actions in the environment.
-
-    Returns:
-        A function that takes the observation as an argument and returns
-        the probabilities for each action in the form of a numpy array of length nA.
-
-    """
-
-    def policy_fn(observation):
-        A = np.ones(nA, dtype=float) * epsilon / nA
-        q_values = estimator.predict(observation)
-        best_action = np.argmax(q_values)
-        A[best_action] += (1.0 - epsilon)
-        return A
-
-    return policy_fn
-
-
 class Estimator(object):
     """
     Value Function approximator.
@@ -138,7 +113,8 @@ def generate_session(env, agent, t_max=int(1e4)):
         total_reward += r
 
         s = new_s
-        if done: break
+        if done:
+            break
     return states, actions, total_reward
 
 
